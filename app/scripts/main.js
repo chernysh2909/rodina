2fbf55093d4f0d1cd26a9d47bfc9f334fc55e77a
chernysh2909 <chernysh2909@gmail.com>
2016-12-21 17:08:40 +0200

    30% work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+    var owl = $('#owl-demo');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        dots: true
+    });
+    // Go to the next item
+    $('#next').click(function() {
+        owl.trigger('next.owl.carousel');
+    });
+    $('#prev').click(function() {
+        owl.trigger('prev.owl.carousel');
+    })
+
+
+
+
+
+
+});
